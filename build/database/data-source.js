adcad6401983bea8e2004deb50d77742d3be8f62
JulioCSSousa <julio.sousa1993@gmail.com>
2024-10-18 03:16:29 -0300

    deploy config

--- build/database/data-source.js
+++ build/database/data-source.js
@@ -32,7 +32,7 @@ dotenv.config();
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "mysql",
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_HOST),
+    port: parseInt(process.env.DB_PORT),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB,
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -11,7 +11,7 @@ dotenv.config();
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_HOST),
+    port: parseInt(process.env.DB_PORT),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB,
